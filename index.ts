e39592553e53de825de27e49441f6908774b9436
Mario Martínez <mario@Marios-Mac-mini.local>
2021-09-09 19:04:53 +0200

    fix to text

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ addEventListener("fetch", async (event) => {
 class ApiController {
   private data: any;
   async getData(): Promise<Response> {
-    this.data = await (await fetch(SOURCE)).json();
+    this.data = await (await fetch(SOURCE)).text();
     return new Response(this.data, { status: 200, headers: {'content-type': 'application/json' }})
   }
 }
