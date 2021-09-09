b4fb5ad9d2973f395cd8bb3e084aba25bacd466d
Mario Martínez <mario@Marios-Mac-mini.local>
2021-09-09 19:02:17 +0200

    testing api

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(
-    new TestController().hello(),
-  );
+const SOURCE = `https://analisi.transparenciacatalunya.cat/resource/a2hm-uzyj.json`;
+
+addEventListener("fetch", async (event) => {
+  const api = new ApiController();
+  event.respondWith(await api.getData());
 });
 
-class TestController {
-
-  hello(): Response {
-    return new Response('hello world martines', { status: 200, headers: {'content-type': 'text/plain' }})
+class ApiController {
+  private data: any;
+  async getData(): Promise<Response> {
+    this.data = (await fetch(SOURCE)).json;
+    return new Response(this.data, { status: 200, headers: {'content-type': 'application/json' }})
   }
 }
