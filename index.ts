dcd852006a0acc8fcec19f9215be3529c587f53f
Mario Martínez <mario@Marios-Mac-mini.local>
2021-10-02 10:28:58 +0200

    add tailwind and improve html

--- index.ts
+++ index.ts
@@ -2,9 +2,19 @@ import { Handlebars } from 'https://deno.land/x/handlebars/mod.ts';
 
 const SOURCE = `https://analisi.transparenciacatalunya.cat/resource/a2hm-uzyj.json`;
 
+type IData = {
+  resum: string;
+  data_pub: string;
+  nom_ens: string;
+  enlla: {
+    url: string;
+  };
+  estat: string;
+}
+
 class ApiController {
 
-  private data: BodyInit = '';
+  private data: IData | BodyInit | undefined = undefined;
 
   async getData(): Promise<Response> {
     this.data = await (await fetch(SOURCE)).text();
@@ -26,11 +36,18 @@ class ViewRender {
     partialsDir: "partials/",
     cachePartials: true,
     defaultLayout: "main",
-    helpers: undefined,
+    helpers: {
+      date: (str: string) => {
+          return new Date(str).toLocaleString('es-ES', { dateStyle: 'short' })
+      },
+    },
     compilerOptions: undefined,
   });
   
   async render(view: string, context: Record<string, unknown>, layout: string) {
+    context.data = (context.data as IData[])
+      .filter((item) => ['TERMINI_PENDENT', 'TERMINI_OBERT'].includes(item.estat))
+      .sort((a: IData, b: IData) => new Date(b.data_pub) - new Date(a.data_pub));
     return await this.engine.renderView(view, context, layout);
   }
 }
@@ -38,7 +55,6 @@ class ViewRender {
 addEventListener("fetch", async (event) => {
   const api = new ApiController();
   event.respondWith(await api.viewData());
-  console.log(await api.viewData());
 });
 
 /* console.log(await new ViewRender().render('index', { data: [{"resum":"4 places de Monitor de natació","data_pub":"2018-04-26T00:00:00.000","enlla":{"url":"http://cido.diba.cat/oposicions/7769693"},"codi_ens":"2500300000","nom_ens":"Ajuntament d'Agramunt","estat":"NO_VIGENT","latitud":"41.7870354","longitud":"1.0988124","geocoded":{"type":"Point","coordinates":[1.0988124,41.7870354]},":@computed_region_bh64_c7uy":"726",":@computed_region_wvic_k925":"17"}] }, 'main'));
